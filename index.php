37f2c220b0e5e3d5369af9e1c0cb39bc59d9efe3
Mateus Joaquim <mateusjoaquim@estudiovh.com.br>
2018-03-21 11:33:04 -0300

    Correcao do console error

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
     <title>Avaliação para Desenvolvimento Web</title>
     <!-- Bootstrap core CSS -->
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
     <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
     <!-- CSS Form validation -->
     <link href="coffe.css" rel="stylesheet">
 
